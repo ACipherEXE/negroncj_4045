c2a1a8f3cee395b42746af73891e42889e1d2895
Administrator <Administrator@DESKTOP-099UQRG>
2022-09-01 15:39:15 -0400

    Initial comment

---
+++ src/main/java/com/palntplaces/PlantPlacesController.java
@@ -0,0 +1,15 @@
+package com.palntplaces;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PlantPlacesController {
+	
+	@RequestMapping("/start")
+	public String start() {
+		return "start";
+		
+	}
+}
+
