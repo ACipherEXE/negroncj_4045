70a588a680da84a9ffc4ca77ecddef8da6ab62f3
Christopher Negron <sonycar2000@outlook.com>
2022-09-06 14:51:20 -0400

    Add Javadoc added default index endpoint

--- src/main/java/com/palntplaces/PlantPlacesController.java
+++ src/main/java/com/palntplaces/PlantPlacesController.java
@@ -5,11 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlantPlacesController {
-	
+	/**
+	 * Handle the /start endpoint
+	 * @return 
+	 */
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
 		
 	}
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+		
+	}
 }
-
